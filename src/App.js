babc409b971ee69f8d4053305f65a4625c7d4300
Cody <relationalprogrammer@gmail.com>
2021-12-07 12:07:27 -0600

    Topic Browser renders from App;

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import TopicBrowser from './components/TopicBrowser/TopicBrowser';
 
 class App extends Component {
   render() {
-    
+      return <TopicBrowser />
   }
 }
 
---
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class TopicBrowser extends Component {
+    render() {
+        return (
+            <p>
+                Hello World.    
+            </p>
+        )
+    }
+}
